87acd5a5134da2dda54ca6742a81e53058c76067
kylec3 <kylec@nakeddev.com>
2021-02-12 16:46:51 -0800

    print and operators

---
+++ Basics/src/App.java
@@ -0,0 +1,12 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        HelloWorld hw = new HelloWorld();
+        hw.Test();
+
+        Operators op = new Operators();
+        op.printAll();
+
+    }
+
+}
---
+++ Basics/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public void Test() {
+        System.out.println();
+        System.out.println("Welcmoe to Java Basics! ");
+        System.out.println("-------------------------- ");
+        System.out.println("Hello, World!");
+    }
+}
