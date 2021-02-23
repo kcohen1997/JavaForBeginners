4c3a608ff74de7159bc30fc385847facc1cf557f
kylec3 <kylec@nakeddev.com>
2021-02-23 13:40:39 -0800

    data types and operators

--- Basics/1. Data Types and Operators/src/App.java
+++ Basics/1. Data Types and Operators/src/App.java
@@ -3,7 +3,7 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         System.out.println();
-        System.out.println("WELCOME TO JAVA BASICS: ");
+        System.out.println("WELCOME TO DATA TYPES AND OPERATORS ");
         System.out.println("------------------------------------------- ");
 
         DataTypes dt = new DataTypes();
@@ -11,12 +11,6 @@ public class App {
 
         Operators op = new Operators();
         op.printAll();
-
-        ObjectTest test = new ObjectTest();
-        test.printEmployees();
-
-        Loops l = new Loops();
-        l.printAll();
 
     }
 
--- Basics/1. Data Types and Operators/src/Operators.java
+++ Basics/1. Data Types and Operators/src/Operators.java
@@ -5,9 +5,19 @@ public class Operators {
         System.out.println("Operators:");
         System.out.println("-------------------------- ");
 
-        System.out.println("Assignment Operators:");
+        System.out.println("Arithmetic Operators:");
         System.out.println();
-        AssignmentOperators();
+        ArithmeticOperators();
+        System.out.println();
+
+        System.out.println("Relational Operators:");
+        System.out.println();
+        RelationalOperators();
+        System.out.println();
+
+        System.out.println("Bitwise Operators:");
+        System.out.println();
+        BitwiseOperators();
         System.out.println();
 
         System.out.println("Logical Operators:");
@@ -15,97 +25,71 @@ public class Operators {
         LogicalOperators();
         System.out.println();
 
-        System.out.println("Relational Operators:");
+        System.out.println("Assignment Operators:");
+        System.out.println();
+        AssignmentOperators();
         System.out.println();
 
-        RelationalOperators();
+        System.out.println("Misc Operators:");
+        System.out.println();
+        MiscOperators();
         System.out.println();
 
     }
 
-    public void AssignmentOperators() {
+    public void ArithmeticOperators() {
         int num1 = 10;
         int num2 = 20;
 
         System.out.println("NUMBER # 1: " + num1 + " NUMBER #2: " + num2);
+        System.out.println();
 
-        num2 = num1;
-        System.out.println("= Output: " + num2);
+        int answer = num1 + num2;
+        System.out.println("+ (Addition) Output: " + answer);
 
-        num2 += num1;
-        System.out.println("+= Output: " + num2);
+        answer = num1 - num2;
+        System.out.println("- (Subtraction) Output: " + answer);
 
-        num2 -= num1;
-        System.out.println("-= Output: " + num2);
+        answer = num1 * num2;
+        System.out.println("* (Multiplication)  Output: " + answer);
 
-        num2 *= num1;
-        System.out.println("*= Output: " + num2);
+        answer = num2 / num1;
+        System.out.println("/ (Division) Output: " + answer);
 
-        num2 /= num1;
-        System.out.println("/= Output: " + num2);
+        answer = num2 % num1;
+        System.out.println("% (Modulus) Output: " + answer);
 
-        num2 %= num1;
-        System.out.println("%= Output: " + num2);
+        answer = num2++;
+        System.out.println("++ (Increment) Output: " + answer);
 
-    }
+        answer = num2--;
+        System.out.println("-- (Decrement) Output: " + answer);
 
-    public void LogicalOperators() {
-        boolean b1 = true;
-        boolean b2 = false;
-        System.out.println("BOOLEAN 1: " + b1 + " BOOLEAN B2: " + b2);
-        System.out.println("b1 && b2: " + (b1 && b2));
-        System.out.println("b1 || b2: " + (b1 || b2));
-        System.out.println("!(b1 && b2): " + !(b1 && b2));
     }
 
     public void RelationalOperators() {
         int num1 = 10;
-        int num2 = 50;
+        int num2 = 20;
         System.out.println("NUMBER # 1: " + num1 + " NUMBER #2: " + num2);
+        System.out.println();
 
-        if (num1 == num2) {
-            System.out.println("num1 and num2 are equal");
-        } else {
-            System.out.println("num1 and num2 are not equal");
-        }
-
-        if (num1 != num2) {
-            System.out.println("num1 and num2 are not equal");
-        } else {
-            System.out.println("num1 and num2 are equal");
-        }
-
-        if (num1 > num2) {
-            System.out.println("num1 is greater than num2");
-        } else {
-            System.out.println("num1 is not greater than num2");
-        }
-
-        if (num1 >= num2) {
-            System.out.println("num1 is greater than or equal to num2");
-        } else {
-            System.out.println("num1 is less than num2");
-        }
-
-        if (num1 < num2) {
-            System.out.println("num1 is less than num2");
-        } else {
-            System.out.println("num1 is not less than num2");
-        }
-
-        if (num1 <= num2) {
-            System.out.println("num1 is less than or equal to num2");
-        } else {
-            System.out.println("num1 is greater than num2");
-        }
+        System.out.println("num1 and num2 are equal: " + (num1 == num2));
+        System.out.println("num1 and num2 are not equal: " + (num1 != num2));
+        System.out.println("num1 is greater than num2: " + (num1 > num2));
+        System.out.println("num1 is less than num2: " + (num1 < num2));
+        System.out.println("num1 is greater than or equal to num2: " + (num1 >= num2));
+        System.out.println("num1 is less than or equal to num2: " + (num1 <= num2));
 
     }
 
     public void BitwiseOperators() {
 
-        int num1 = 11; /* 11 = 00001011 */
-        int num2 = 22; /* 22 = 00010110 */
+        int num1 = 60; /* 11 = 00001011 */
+        int num2 = 13; /* 22 = 00010110 */
         int result = 0;
+
+        System.out.println("NUMBER # 1: " + num1 + " NUMBER #2: " + num2);
+        System.out.println();
 
         result = num1 & num2;
         System.out.println("num1 & num2: " + result);
@@ -123,5 +107,77 @@ public class Operators {
         System.out.println("num1 << 2: " + result);
         result = num1 >> 2;
         System.out.println("num1 >> 2: " + result);
+
+        result = num1 >>> 2;
+        System.out.println("num1 >>> 2: " + result);
     }
+
+    public void LogicalOperators() {
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println("BOOLEAN 1: " + b1 + " BOOLEAN B2: " + b2);
+        System.out.println();
+        System.out.println("b1 && b2: " + (b1 && b2));
+        System.out.println("b1 || b2: " + (b1 || b2));
+        System.out.println("!(b1 && b2): " + !(b1 && b2));
+    }
+
+    public void AssignmentOperators() {
+        int num1 = 10;
+        int num2 = 20;
+        int answer = 0;
+
+        System.out.println("NUMBER # 1: " + num1 + " NUMBER #2: " + num2);
+        System.out.println();
+
+        num2 = num1;
+        System.out.println("= Output: " + num2);
+
+        System.out.println();
+        System.out.println("NUMBER # 1: " + num1 + " NUMBER #2: " + num2);
+
+        answer = num2 += num1;
+        System.out.println("+= Output: " + answer);
+
+        answer = num2 -= num1;
+        System.out.println("-= Output: " + answer);
+
+        answer = num2 *= num1;
+        System.out.println("*= Output: " + answer);
+
+        answer = num2 /= num1;
+        System.out.println("/= Output: " + answer);
+
+        answer = num2 %= num1;
+        System.out.println("%= Output: " + answer);
+
+        answer = num2 <<= num1;
+        System.out.println("<<= Output: " + answer);
+
+        answer = num2 >>= num1;
+        System.out.println(">>= Output: " + answer);
+
+        answer = num2 &= num1;
+        System.out.println("&= Output: " + answer);
+
+        answer = num2 ^= num1;
+        System.out.println("^= Output: " + answer);
+
+        answer = num2 |= num1;
+        System.out.println("!= Output: " + answer);
+
+    }
+
+    public void MiscOperators() {
+
+        int num1 = 10;
+        int num2 = 20;
+        boolean answer = num1 == num2 ? true : false;
+        System.out.println("NUMBER # 1: " + num1 + " NUMBER #2: " + num2);
+        System.out.println("Conditional Operator: is number 1 equal to number 2? " + answer);
+        String name = "James";
+        boolean result = name instanceof String;
+        System.out.println("Instanceof: is James a string? " + result);
+    }
+
 }
